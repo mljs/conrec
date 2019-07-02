3341203c2c4582dacd663741d3d973da857a7f27
Michaël Zasso <targos@protonmail.com>
2019-07-02 22:51:35 +0200

    feat: add a swapAxes option and swap the default axes (#6)
    BREAKING CHANGE:
    
    In previous versions, the X axis was along the rows of the data matrix
    and the Y axis along the columns. This was not intuitive regarding how
    the contours are then drawn. The default was changed to now consider X
    along the columns and Y along the rows respectively.
    Pass `swapAxes: true` to the Conrec constructor to get the previous
    behavior.

--- src/BasicContourDrawer.js
+++ src/BasicContourDrawer.js
@@ -1,16 +1,21 @@
 export class BasicContourDrawer {
-  constructor(levels) {
+  constructor(levels, swapAxes) {
     this.contour = new Array(levels.length);
     for (var i = 0; i < levels.length; i++) {
       this.contour[i] = {
         zValue: levels[i],
-        lines: []
+        lines: [],
       };
     }
+    this.swapAxes = swapAxes;
   }
 
   drawContour(x1, y1, x2, y2, z, k) {
-    this.contour[k].lines.push(x1, y1, x2, y2);
+    if (!this.swapAxes) {
+      this.contour[k].lines.push(y1, x1, y2, x2);
+    } else {
+      this.contour[k].lines.push(x1, y1, x2, y2);
+    }
   }
 
   getContour() {
--- src/__tests__/conrec.test.js
+++ src/__tests__/conrec.test.js
@@ -4,29 +4,30 @@ import converter from 'jcampconverter';
 
 import { Conrec } from '..';
 
-describe('conrec basic test', () => {
-  const data = fs.readFileSync(`${__dirname}/data/zhmbc_0.jdx`, 'utf8');
-  const parsed = converter.convert(data, { noContour: true });
-  const conrec = new Conrec(parsed.minMax.z);
+const data = fs.readFileSync(`${__dirname}/data/zhmbc_0.jdx`, 'utf8');
+const parsed = converter.convert(data, { noContour: true });
 
+describe('conrec basic test', () => {
   it('no result because level too far', () => {
+    const conrec = new Conrec(parsed.minMax.z);
     const basic = conrec.drawContour({
       contourDrawer: 'basic',
       levels: [-1000000000, 1000000000],
-      timeout: 10000
+      timeout: 10000,
     });
 
     expect(basic).toStrictEqual([
       { lines: [], zValue: -1000000000 },
-      { lines: [], zValue: 1000000000 }
+      { lines: [], zValue: 1000000000 },
     ]);
   });
 
   it('2 specified levels', () => {
+    const conrec = new Conrec(parsed.minMax.z);
     const basic = conrec.drawContour({
       contourDrawer: 'basic',
       levels: [-100000, 100000],
-      timeout: 10000
+      timeout: 10000,
     });
 
     expect(basic).toHaveLength(2);
@@ -35,10 +36,27 @@ describe('conrec basic test', () => {
   });
 
   it('auto select levels', () => {
+    const conrec = new Conrec(parsed.minMax.z);
     const basic = conrec.drawContour({
       contourDrawer: 'basic',
       nbLevels: 10,
-      timeout: 10000
+      timeout: 10000,
+    });
+
+    expect(basic).toHaveLength(10);
+    expect(basic[0].lines).toHaveLength(0);
+    expect(basic[1].lines).toHaveLength(4984);
+    expect(basic[8].lines).toHaveLength(32);
+    expect(basic[8].lines).toMatchSnapshot();
+    expect(basic[9].lines).toHaveLength(0);
+  });
+
+  it('auto select levels with swapAxes', () => {
+    const conrec = new Conrec(parsed.minMax.z, { swapAxes: true });
+    const basic = conrec.drawContour({
+      contourDrawer: 'basic',
+      nbLevels: 10,
+      timeout: 10000,
     });
 
     expect(basic).toHaveLength(10);
