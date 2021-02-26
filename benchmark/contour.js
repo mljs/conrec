aa665477db0b6798edfc1a137a19a4851521b0a1
Luc Patiny <luc@patiny.com>
2021-02-26 16:15:33 +0100

    chore: add benchmark

---
+++ benchmark/contour.js
@@ -0,0 +1,36 @@
+import { readFileSync } from 'fs';
+import { join } from 'path'
+
+import converter from 'jcampconverter';
+
+import { Conrec } from '../src/index.js';
+
+const data = readFileSync(join(__dirname, '../src/__tests__/data/zhmbc_0.jdx'), 'utf8');
+const parsed = converter.convert(data, { noContour: true });
+
+for (let i = 0; i < parsed.minMax.z.length; i++) {
+  parsed.minMax.z[i] = Float64Array.from(parsed.minMax.z[i])
+}
+
+const conrec = new Conrec(parsed.minMax.z);
+
+console.time('basic');
+for (let i = 0; i < 20; i++) {
+
+  conrec.drawContour({
+    contourDrawer: 'basic',
+    levels: [-100000, 100000],
+    timeout: 10000,
+  });
+}
+console.timeEnd('basic');
+
+console.time('shape');
+for (let i = 0; i < 20; i++) {
+  conrec.drawContour({
+    contourDrawer: 'shape',
+    levels: [-1000000000, 1000000000],
+    timeout: 10000,
+  });
+}
+console.timeEnd('shape');
