295b4151801967061a27eea61fd17918bca58c8c
Wadjih Bencheikh <jw_bencheikh@esi.dz>
2023-07-03 22:19:11 +0100

    test: add Float64Array matrix (#22)

--- src/__tests__/shape.test.ts
+++ src/__tests__/shape.test.ts
@@ -58,6 +58,23 @@ describe('shape', () => {
     expect(timeout).toBeFalsy();
     expect(contours[0].lines).toHaveLength(49);
   });
+  it('square float matrix', () => {
+    const matrix: Float64Array[] = [
+      Float64Array.from([0, 0, 0.1, 0, 0]),
+      Float64Array.from([0, 0.9, 1, 1, 0]),
+      Float64Array.from([0, 1, 1, 1, 0]),
+      Float64Array.from([0, 1, 1, 0.8, 0]),
+      Float64Array.from([0, 0, 0, 0, 0.1]),
+    ];
+
+    const conrec = new Conrec(matrix);
+    const { contours, timeout } = conrec.drawContour({
+      contourDrawer: 'shape',
+      levels: [0.5],
+    });
+    expect(timeout).toBeFalsy();
+    expect(contours[0].lines).toHaveLength(29);
+  });
 });
 
 function parse(string: string) {
